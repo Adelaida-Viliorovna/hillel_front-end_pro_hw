94e578f0b40dc471475897be1c583930724574c9
Adelaida-Viliorovna <adelaida.viliorovna@gmail.com>
2024-10-20 14:20:45 +0300

    add hw_3-2 files

---
+++ index.js
@@ -0,0 +1,16 @@
+// v1
+const name1 = "Anastasiia";
+const age1 = 22;
+const movie1 = "Star Wars";
+
+const message1 =`Your name - ${name1}, Your age - ${age1}, Your favorite movie - "${movie1}"`;
+console.log(message1);
+
+// v2
+const name2 = prompt("Your name:");
+const age2 = prompt("Your age:");
+const movie2 = prompt("Your favorite movie:");
+
+const message2 = `Your name - ${name2}, Your age - ${age2}, Your favorite movie - "${movie2}"`;
+console.log(message2);
+alert(message2);
