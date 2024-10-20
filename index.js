b3880fbc78e13c8d69ad0142df6b431ff6bd626b
Adelaida-Viliorovna <adelaida.viliorovna@gmail.com>
2024-10-20 15:20:43 +0300

    upd index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const number = prompt("Enter a five-digit number:");
-const message1 = `Your number: ${number}`;
 
 if (number.length === 5 && !isNaN(number)) {
     const number2 = number.split('').join(' ');
