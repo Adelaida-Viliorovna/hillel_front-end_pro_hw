f6ab374cd767bbf5c09e944b587f8fcd4bab1603
Adelaida-Viliorovna <adelaida.viliorovna@gmail.com>
2024-11-18 17:18:24 +0200

    upd js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const randomButton = document.getElementById('random-btn');
 const img = document.createElement('img');
 
 randomButton.addEventListener('click', () => {
-    const randomIndex = Math.floor(Math.random() * 9) + 1;
+    const randomIndex = Math.floor(Math.random() * 10) + 1;
     img.src = `./img/${randomIndex}.png`;
     img.alt = 'Random img';
     img.style.height = '400px';
