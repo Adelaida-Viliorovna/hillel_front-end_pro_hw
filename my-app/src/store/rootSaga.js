c581e65798d2e72a95ea7b1472d1e4b5c1af5358
Adelaida-Viliorovna <adelaida.viliorovna@gmail.com>
2025-02-17 16:12:22 +0200

    upd files

--- my-app/src/store/rootSaga.js
+++ my-app/src/store/rootSaga.js
@@ -1,8 +1,8 @@
 // my-app\src\store\rootSaga.js
 
-import { all } from "redux-saga/effects";
+import { all, call } from "redux-saga/effects";
 import { todoWatcher } from "./watchers";
 
 export default function* rootSaga() {
-  yield all([todoWatcher()]);
+  yield all([call(todoWatcher),]);
 }
--- my-app/src/store/watchers.js
+++ my-app/src/store/watchers.js
@@ -1,12 +1,12 @@
 // my-app\src\store\watchers.js
 
-import { takeLatest } from "redux-saga/effects";
+import { takeEvery, takeLatest } from "redux-saga/effects";
 import { fetchTodos, addTodoSaga, removeTodoSaga, clearTodosSaga, toggleCompleteTodoSaga } from "./workers";
 
 export function* todoWatcher() {
-  yield takeLatest("FETCH_TODOS", fetchTodos);
-  yield takeLatest("ADD_TODO_REQUEST", addTodoSaga);
-  yield takeLatest("REMOVE_TODO_REQUEST", removeTodoSaga);
-  yield takeLatest("CLEAR_TODOS_REQUEST", clearTodosSaga);
-  yield takeLatest("TOGGLE_COMPLETE_TODO", toggleCompleteTodoSaga);
+  yield takeEvery("FETCH_TODOS", fetchTodos);
+  yield takeEvery("ADD_TODO_REQUEST", addTodoSaga);
+  yield takeEvery("REMOVE_TODO_REQUEST", removeTodoSaga);
+  yield takeEvery("CLEAR_TODOS_REQUEST", clearTodosSaga);
+  yield takeEvery("TOGGLE_COMPLETE_TODO", toggleCompleteTodoSaga);
 }
